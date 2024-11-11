9c9ed9c11a48c1a846a8af540ce0d13357dd052c
AutisticIntelligence1 <AutisticIntelligence100@proton.me>
2024-11-11 09:35:22 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone',
+    poweredByHeader: false,
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    eslint: {
+        ignoreDuringBuilds: true
+    }
 }
+
+module.exports = nextConfig
